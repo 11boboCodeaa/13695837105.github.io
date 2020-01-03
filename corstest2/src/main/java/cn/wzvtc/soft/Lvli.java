9d47c961feed70fbcf33c289a5effd0120f2fe11
13695837105 <43591824+13695837105@users.noreply.github.com>
2020-01-03 08:53:20 +0800

    Add files via upload

---
+++ corstest2/src/main/java/cn/wzvtc/soft/Lvli.java
@@ -0,0 +1,37 @@
+package cn.wzvtc.soft;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Lvli {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String number;
+
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Lvli(){
+
+    }
+    public Lvli(String number, String url) {
+        this.number = number;
+        this.url = url;
+    }
+}
